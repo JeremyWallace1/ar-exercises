90b665eb4aade1e590ddc4626424137bd417df76
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-23 15:56:30 -0700

    add three stores and confirm Store.count using ActiveRecord count method

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,10 +4,31 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-# Exercise 1: Create 3 stores
+### Exercise 1: Create 3 stores ###
 # Use Active Record's create class method multiple times to create 3 stores in the database:
-# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-# Richmond (annual_revenue of 1260000 carries women's apparel only)
-# Gastown (annual_revenue of 190000 carries men's apparel only)
-# Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
+#--> Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+burnaby = Store.new
+burnaby.name = "Burnaby"
+burnaby.annual_revenue = 300000
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+burnaby.save
 
+#--> Richmond (annual_revenue of 1260000 carries women's apparel only)
+richmond = Store.new
+richmond.name = "Richmond"
+richmond.annual_revenue = 1260000
+richmond.mens_apparel = false
+richmond.womens_apparel = true
+richmond.save
+
+#--> Gastown (annual_revenue of 190000 carries men's apparel only)
+gastown = Store.new
+gastown.name = "Gastown"
+gastown.annual_revenue = 190000
+gastown.mens_apparel = true
+gastown.womens_apparel = false
+gastown.save
+
+# Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
+puts Store.count
