472c1496badc67f9e4631555a9a91aa1afdc7bf7
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-23 18:43:00 -0700

    add validations for store.rb and employee.rb

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
   validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :mens_apparel_or_womens_apparel
+
+  def mens_apparel_or_womens_apparel
+    unless mens_apparel || womens_apparel
+      errors.add("mens_apparel or womens_apparel must be true")
+    end
+  end
+
+
 end
