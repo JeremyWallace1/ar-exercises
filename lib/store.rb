34ccb5236868e00e3ef8837d78357c1e5d9a8212
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-24 18:16:55 -0700

    add private method to store.rb to validate destroying a store

--- lib/store.rb
+++ lib/store.rb
@@ -1,8 +1,10 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  validates :name, presence: true, length: { minimum: 3 }
-  validates :annual_revenue, numericality: { greater_than: 0 }
-  validate :mens_apparel_or_womens_apparel
+  # validates :name, presence: true, length: { minimum: 3 }
+  # validates :annual_revenue, numericality: { greater_than: 0 }
+  # validate :mens_apparel_or_womens_apparel
+
+  before_destroy :can_destroy?
 
   def mens_apparel_or_womens_apparel
     unless mens_apparel || womens_apparel
@@ -10,5 +12,11 @@ class Store < ActiveRecord::Base
     end
   end
 
-
+  private
+    def can_destroy?
+      if self.annual_revenue > 0 || self.employees.size > 0
+        self.errors.add(:base, "Can't be destroyed because either annual revenue > $0 or more than 0 employees.")
+        throw :abort
+      end
+    end
 end
